2239fa47dd06c8ac0f906188416a7889f27b1a28
LucasEPaduam <79526125+LucasEPaduam@users.noreply.github.com>
2022-07-11 19:18:10 -0300

    First component

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,14 @@
+import NotificationButton from "./components/notificationButton"
+
+
+
 
 function App() {
   return (
-    <h1>OLÁ MUNDO</h1>
+    <>
+      <h1>OLÁ MUNDO</h1>
+      <NotificationButton />
+    </>
   )
 }
 
